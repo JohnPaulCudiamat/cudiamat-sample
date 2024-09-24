ab96367b866381e281bf101557f34f2728035daf
JohnPaulCudiamat <cdmt245@gmail.com>
2024-09-24 09:17:56 +0800

    first upload or  first file

---
+++ Laboratory.py
@@ -0,0 +1,14 @@
+first_name = "John Paul"
+last_name = "Cudiamat"
+Age = 19
+
+fullname = first_name + " " + last_name
+print("Hello, my name is" + " " + first_name + " " + last_name, "I am", 19, "years old")
+
+
+if (fullname.isupper == True):
+    print(fullname)
+
+else:
+    new_full = fullname.upper()
+    print(new_full)
---
+++ hello.py
@@ -0,0 +1,13 @@
+#This command is for displaying output
+
+print("Hello World")
+
+first_name = "John Paul"
+last_name = "Cudiamat"
+
+print(first_name)
+print(last_name)
+
+print(first_name, last_name)
+print(first_name + last_name)
+print(first_name + " " + last_name)
